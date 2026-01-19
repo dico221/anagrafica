9f64d3f4510d85051517d6eff94551cc68a80aa7
dico221 <238628284+dico221@users.noreply.github.com>
2026-01-19 21:12:13 +0100

    modifica 5.0

--- 5.0/js/script.js
+++ 5.0/js/script.js
@@ -4,12 +4,20 @@ function funzione() {
         nome: document.getElementById("nome").value,
         cognome: document.getElementById("cognome").value
     }
-    if(persona.nome != "" && persona.cognome != ""){
+    if(persona.nome.trim() != "" && persona.cognome.trim() != ""){
         localStorage.setItem("persona" + localStorage.length.toString(), JSON.stringify(persona))
         window.location.href = "./stampa.html"}
     else {
         warning.style.color = "red"
-        warning.innerHTML = "Inserire nome e cognome"
+        if(persona.nome.trim() == "" && persona.cognome.trim() != ""){
+            warning.innerHTML = "Inserire nome"
+        }
+        else if(persona.cognome.trim() == "" && persona.nome.trim() != ""){
+            warning.innerHTML = "Inserire cognome"
+        }
+        else{
+            warning.innerHTML = "Inserire nome e cognome"
+        }
     }
     
 }
